17112ff799881a08b15a87c67316ae89a343ce6c
Dark1913 <109271843+Dark1913@users.noreply.github.com>
2022-11-19 18:25:18 +0000

    Update __main__.py

--- VCsMusicBot/__main__.py
+++ VCsMusicBot/__main__.py
@@ -1,10 +1,10 @@
 import requests
 from pyrogram import Client as Bot
 
-from VCsMusicBot.config import API_HASH
-from VCsMusicBot.config import API_ID
+from VCsMusicBot.config import 8b2c2204694f06a9ca85047ec4f531a8
+from VCsMusicBot.config import 21434130
 from VCsMusicBot.config import BG_IMAGE
-from VCsMusicBot.config import BOT_TOKEN
+from VCsMusicBot.config import 5542726899:AAHOAOHZzqXtzqScNdyEh-t7v5sAwF1J84Q
 from VCsMusicBot.services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
@@ -14,9 +14,9 @@ file.close()
 
 bot = Bot(
     ":memory:",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
+    21434130,
+    8b2c2204694f06a9ca85047ec4f531a8,
+    AAHOAOHZzqXtzqScNdyEh-t7v5sAwF1J84Q,
     plugins=dict(root="VCsMusicBot.modules"),
 )
 
